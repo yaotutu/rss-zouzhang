464a0a403f3eca1742cb6a93257a59eaa340ff87
yaotutu <yaotutu@gmail.com>
2024-08-17 11:25:44 +0800

    * feat: add .env file with DATABASE_URL configuration * feat: add @prisma/client and prisma dependencies to package.json * feat: add @prisma/client and prisma dependencies to pnpm-lock.yaml * feat: add dev.db file * feat: add dev.db-journal file * feat: add migration.sql file * feat: add migration_lock.toml file * feat: add schema.prisma file

---
+++ prisma/migrations/20240817025544_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "rssContent" TEXT NOT NULL,
+    "periodIndex" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
